462811ca74ede2e85651dd067dcb29fdd6dfca12
AlexR <anroebuck@googlemail.com>
2024-06-10 23:35:15 +0100

    add new logos and sellers

--- src/utils/sellers.js
+++ src/utils/sellers.js
@@ -1,20 +1,28 @@
 import axionLogo from '../assets/axion 150x60.png';
-import bigOrbitLogo from '../assets/bigOrbitCards 150x60.png';
+// import bigOrbitLogo from '../assets/bigOrbitCards 150x60.png';
+import boardsAndSwordsLogo from '../assets/boardsAndSwords 5to2.png';
 // import chaosLogo from '../assets/chaosCards 150x60.png';
+import diceSaloonLogo from '../assets/diceSaloonSingles 500x200.png';
+import doubleSleevedLogo from '../assets/doubleSleeved 5to2.png';
+import gameHQLogo from '../assets/gameHQ 250x100.png';
+import gatheringPoingLogo from '../assets/gatheringPointGames 450x180.png';
+// import hareruyaLogo from '../assets/hareryua 150x60.png';
 import harlequinsLogo from '../assets/harlequins 150x60.png';
-import hareruyaLogo from '../assets/hareryua 150x60.png';
-import lazyDragonLogo from '../assets/lazyDragonGaming-3 150x60.jpg';
+import highlanderGamesLogo from '../assets/highlanderGames 5to2 b.png';
+// import lazyDragonLogo from '../assets/lazyDragonGaming-3 150x60.jpg';
 import londonMagicLogo from '../assets/londonMagicTraders 150x60.png';
 import lvlUpLogo from '../assets/lvlUp 150x60.png';
 import magicCardTraderLogo from '../assets/magicCardTrader-black 150x60.png';
 import magicMadhouseLogo from '../assets/magicMadhouse 150x60.png';
+import manaGamingLogo from '../assets/manaGaming 150x60.png';
 import manaleakLogo from '../assets/manaLeak 150x60.png';
 // import mkmLogo from '../assets/mkm 150x60.png';
-import mountBattenLogo from '../assets/mountBatten 150x60.png';
-import nerdShakLogo from '../assets/nerdShak 150x60.jpg';
+// import mountBattenLogo from '../assets/mountBatten 150x60.png';
+// import nerdShakLogo from '../assets/nerdShak 150x60.jpg';
 import pgLeedsLogo from '../assets/patriotGamesLeeds 150x60.png';
-import starCityGamesLogo from '../assets/starCityGames 150x60.png';
-import trollLogo from '../assets/trollTrader 150x60.png';
+import skywardFireLogo from '../assets/skywardFire 790x316.png';
+import starCityGamesLogo from '../assets/starCityGames-new 250x100.png';
+import trollLogo from '../assets/trollTrader-new 600x120.png';
 // import unionCountyLogo from '../assets/unionCountyGames 150x60.png';
 
 import { currency } from './enums';
@@ -48,34 +56,49 @@ export const sellers = {
     logo: axionLogo,
     currency: currency.GBP,
   },
-  bigOrbit: {
-    name: 'Big Orbit Cards',
-    logo: bigOrbitLogo,
+  boardsAndSwords: {
+    name: 'Boards and Swords',
+    logo: boardsAndSwordsLogo,
     currency: currency.GBP,
   },
-  // chaos: {
-  //   name: 'Chaos Cards',
-  //   logo: chaosLogo,
-  //   currency: currency.GBP,
-  // },
+  diceSaloon: {
+    name: 'Dice Saloon',
+    logo: diceSaloonLogo,
+    currency: currency.GBP,
+  },
+  doubleSleeved: {
+    name: 'Double Sleeved',
+    logo: doubleSleevedLogo,
+    currency: currency.GBP,
+  },
+  gameHQ: {
+    name: 'Game HQ',
+    logo: gameHQLogo,
+    currency: currency.GBP,
+  },
+  gatheringPoint: {
+    name: 'Gathering Point Games',
+    logo: gatheringPoingLogo,
+    currency: currency.GBP,
+  },
   harlequins: {
     name: 'Harlequins',
     logo: harlequinsLogo,
     currency: currency.GBP,
   },
-  // lazyDragon : {
-  //   name: 'Lazy Dragon Gaming',
-  //   logo: lazyDragonLogo,
-  //   currency: currency.GBP,
-  // },
-  lvlUp: {
-    name: 'Lvl Up Gaming',
-    logo: lvlUpLogo,
+  highlanderGames: {
+    name: 'Highlander Games',
+    logo: highlanderGamesLogo,
     currency: currency.GBP,
   },
   londonMagic: {
     name: 'London Magic Traders',
     logo: londonMagicLogo,
+    currency: currency.GBP,
+  },
+  lvlUp: {
+    name: 'Lvl Up Gaming',
+    logo: lvlUpLogo,
     currency: currency.GBP,
   },
   magicCardTrader: {
@@ -88,24 +111,14 @@ export const sellers = {
     logo: magicMadhouseLogo,
     currency: currency.GBP,
   },
+  manaGaming: {
+    name: 'Mana Gaming',
+    logo: manaGamingLogo,
+    currency: currency.GBP,
+  },
   manaLeak: {
     name: 'Manaleak',
     logo: manaleakLogo,
-    currency: currency.GBP,
-  },
-  // mkm: {
-  //   name: 'Magic Card Market',
-  //   logo: mkmLogo,
-  //   currency: currency.EUR,
-  // },
-  mountBatten: {
-    name: 'Mountbatten Collectables',
-    logo: mountBattenLogo,
-    currency: currency.GBP,
-  },
-  nerdShak: {
-    name: 'Nerd Shak',
-    logo: nerdShakLogo,
     currency: currency.GBP,
   },
   pgLeeds: {
@@ -113,24 +126,19 @@ export const sellers = {
     logo: pgLeedsLogo,
     currency: currency.GBP,
   },
+  skywardFire: {
+    name: 'Skyward Fire',
+    logo: skywardFireLogo,
+    currency: currency.GBP,
+  },
   trollTrader: {
     name: 'Troll Trader',
     logo: trollLogo,
     currency: currency.JPY,
   },
-  // unionCounty: {
-  //   name: 'Union County Games',
-  //   logo: unionCountyLogo,
-  //   currency: currency.GBP,
-  // },
 
   // Non-UK
 
-  hareruya: {
-    name: 'Hareruya',
-    logo: hareruyaLogo,
-    currency: currency.JPY,
-  },
   starCityGames: {
     name: 'Star City Games',
     logo: starCityGamesLogo,
